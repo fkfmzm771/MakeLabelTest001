f1fa8793334e60bd51821e00bfaaa2cce5c679e4
JAPAN KLEEN-TEX <ktjpwuser@kleen-tex.jp>
2019-12-17 17:09:08 +0900

    Add project files.

---
+++ MakeLabelTest001/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MakeLabelTest001.Models;
+using Microsoft.Identity.Client;
+
+namespace MakeLabelTest001.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+      
+            var clientId = "2244807b-abce-4703-8198-0323f7bc8b51";
+
+            string[] scopes = new string[] { "name.read" };
+            var app = PublicClientApplicationBuilder.Create(clientId).Build();
+            Debug.WriteLine("--");
+            Debug.WriteLine(app.AcquireTokenInteractive(scopes) + " test=========================== ");
+            Debug.WriteLine("--");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
